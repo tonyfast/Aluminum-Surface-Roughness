04d50f54438068edfd8af1a4af23415590ff474c
Tony Fast <tony.fast@gmail.com>
2014-05-20 11:51:28 -0400

    Converters for both datasets added and integrated into the driver

---
+++ ConvertSurfaceHPA.m
@@ -0,0 +1,50 @@
+function data = ConvertSurfaceHPA( file, param )
+% Convert Mark Stoudt's data to webpages
+
+if nargin == 1
+    param = struct('plotpre','');
+end
+
+data.surfaceheight = importdata( file );
+
+%% Unbiased effective statistical measure
+data.averageheight = mean(data.surfaceheight(:));
+data.stdheight = std(data.surfaceheight(:));
+
+% Add higher statistics moments because distributions look skew
+data.kurtheight = kurtosis(data.surfaceheight(:));
+data.skewnessheight = skewness(data.surfaceheight(:));
+
+data.maxheight = max(data.surfaceheight(:));
+data.minheight = min(data.surfaceheight(:));
+
+
+%% Visualize data
+
+if numel(param.plotpre)>0
+    pcolor( data.surfaceheight );
+    axis equal; shading flat;
+
+    hc = colorbar;
+    set( get( hc,'ylabel'),'String', 'Surface Height','VerticalAlignment','Bottom', 'Fontsize',16);
+    xlabel( 'microns','Fontsize',16); ylabel( 'microns','Fontsize',16);
+    
+    saveas( gcf,sprintf('./assets/%s-1.png', param.plotpre));
+    data.image{1} = sprintf('%s-1.png', param.plotpre);
+    
+    [ yy xx ] = hist( data.surfaceheight(:), 101 );
+    plot( xx, yy./sum(yy),'o--k','LineWidth',3)
+    xlabel( 'height in microns','Fontsize',16); 
+    ylabel( 'probability','Fontsize',16); 
+    saveas( gcf,sprintf('./assets/%s-2.png', param.plotpre));
+    data.image{2} = sprintf('%s-2.png', param.plotpre);
+end
+%% Parse experiment metadata
+experimentmeta = strsplit( file,filesep);
+
+
+% Step in HPA strain experiment
+data.step = str2num(regexprep(regexprep(experimentmeta{end},'Strain',''),'F.txt',''));
+data.name = regexprep(regexprep(experimentmeta{end},'Strain',''),'F.txt','')
+
+%% Shift the height data to make an image
